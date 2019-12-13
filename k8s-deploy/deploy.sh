08cd1a1354093dd8222cc4d28910e521aa1a54ab
Guilherme Almeida <Guilherme.Almeida@KPMG.co.uk>
2019-12-13 15:06:09 +0000

    1st version included diagram

--- k8s-deploy/deploy.sh
+++ k8s-deploy/deploy.sh
@@ -1,3 +1,2 @@
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml
-#kubectl edit deployment.apps/alb-ingress-controller -n kube-system
+kubectl apply -f rbac-role.yaml
+kubectl apply -f alb-ingress-controller.yaml
